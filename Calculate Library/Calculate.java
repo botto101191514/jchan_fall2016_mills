d6bbb837afffe5f4bfb82ccfad141a38e206d574
botto101191514 <jasonchan117@gmail.com>
2016-08-31 19:33:52 -0700

    math libraries

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -3,4 +3,16 @@ public class Calculate {
 	public static double square (double x) {
 		return x * x;
 	}
+	public static double cube (double x) {
+		return x * x * x;
+	}
+	public static double average2(double x, double y) {
+		return (x+y)/2;	
+	}
+	public static double average3(double x, double y, double z) {
+		return (x+y+z)/2;	
+	}
+	public static double toDegrees(double x) {
+		return (3.14159 / x) * (180 / 3.14159);
+	}
 }

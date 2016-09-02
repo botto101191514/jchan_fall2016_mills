71f9b33e43e9da1c44258c08a704be79f55054f1
botto101191514 <jasonchan117@gmail.com>
2016-09-01 20:19:22 -0700

    calc lib

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -13,6 +13,18 @@ public class Calculate {
 		return (x+y+z)/2;	
 	}
 	public static double toDegrees(double x) {
-		return (3.14159 / x) * (180 / 3.14159);
+		return (3.14159 / x) * (180 / 3.14159);	
+	}
+	public static double toRadians(double x) {
+		return x * (3.14159/180);
+	}
+	public static double discriminant(double x, double y, double z) {
+		return (x * x) - (4 * y * z); 
+	}
+	public static double toImproperFrac(int x, int y, int z) {
+		return ((z * x) + y);
+	}
+	public static double toMixedNum(int x, int y){
+		return ((x / y) );
 	}
 }
